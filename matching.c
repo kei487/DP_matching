3aafba1294a983b8e13f72159e2cf41e489f40c4
Keitaro Nakamrua <s22c1093dj@s.chibakoudai.jp>
2024-05-21 11:14:03 +0900

    add code for A*

--- matching.c
+++ matching.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 #define MAX_N 140
+typedef struct{
+    int data;
+    int from_x,from_y;
+    int flag;
+    double scr_r,scr_d,scr_rd; 
+}MAP;
+
+
 
 int main(){
     //variable initialize
@@ -11,8 +19,7 @@ int main(){
     char fail1[30]="data/city021/city021_001.txt";
     char fail2[30]="data/city011/city011_001.txt";
     double I[MAX_N][15],J[MAX_N][15];
-    double map[MAX_N][MAX_N];
-
+    MAP maps[MAX_N][MAX_N];
 
     //Loop1
     for(loop1=0;loop1<1;loop1++){
@@ -26,7 +33,7 @@ int main(){
         fscanf(fph,"%d",&n_i);
         for(i=0;i<n_i;i++){
             for(j=0;j<15;j++){
-                fscanf(fph,"%lf",&I[i][j]);
+                fscanf(fph,"%lf", &I[i][j]);
             }
 	    }
 
@@ -49,10 +56,24 @@ int main(){
             //mapping
             for(i=0;i<n_i;i++){
                 for(j=0;j<n_j;j++){
-                    map[i][j]=0;
+                    maps[i][j].data=0;
                     for(k=0;k<15;k++){
-                        map[i][j] += (I[i][k]-J[j][k])*(I[i][k]-J[j][k]);
-                    }
+                        maps[i][j].data += (I[i][k]-J[j][k])*(I[i][k]-J[j][k]);
+                    }    
+                    maps[i][j].flag=0;
+                }
+            }
+            int tmp_x=0,tmp_y=0;
+            int goal_x=n_i-1,goal_y=n_j-1;
+            while(tmp_x!=goal_x||tmp_y!=goal_y){
+                if(tmp_x==goal_x){
+                    maps[tmp_x][tmp_y].scr_d = maps[tmp_x][tmp_y+1].data + (goal_y-(tmp_y+1))*(goal_y-(tmp_y+1));
+                }else if(tmp_y==goal_y){
+                    maps[tmp_x][tmp_y].scr_r = maps[tmp_x+1][tmp_y].data + (goal_x-(tmp_x+1))*(goal_x-(tmp_x+1));
+                }else{
+                    maps[tmp_x][tmp_y].scr_d = maps[tmp_x][tmp_y+1].data + (goal_x-(tmp_x))*(goal_x-(tmp_x)) + (goal_y-(tmp_y+1))*(goal_y-(tmp_y+1));
+                    maps[tmp_x][tmp_y].scr_r = maps[tmp_x+1][tmp_y].data + (goal_x-(tmp_x+1))*(goal_x-(tmp_x+1)) + (goal_y-(tmp_y))*(goal_y-(tmp_y));
+                    maps[tmp_x][tmp_y].scr_rd = 2*maps[tmp_x+1][tmp_y+1].data + (goal_x-(tmp_x+1))*(goal_x-(tmp_x+1)) + (goal_y-(tmp_y+1))*(goal_y-(tmp_y+1));
                 }
             }
 
@@ -84,7 +105,7 @@ int main(){
         //output
         for(i=0;i<n_i;i++){
             for(j=0;j<n_j;j++){
-                printf("%5.5lf ",map[i][j]);
+                printf("%5.5lf ",maps[i][j].data);
             }
             printf("\n");
         }
